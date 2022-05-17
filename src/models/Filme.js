bcc12ae7f0c5202e14346007edd010b7d80f335d
Yuri Felipe Scabio <102414254+StaticDreamstate@users.noreply.github.com>
2022-05-17 14:35:31 -0300

    Bug Fix

--- src/models/Filme.js
+++ src/models/Filme.js
@@ -1,44 +1,33 @@
-
 const { DataTypes } = require("sequelize");
+
 const db = require("../database");
 
-const Filme = db.define("Filme", {
-
+const Filme = db.define(
+  "Filme",
+  {
     codigo: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-
     nome: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-
     ano_lancamento: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-
     duracao: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-
+      type: DataTypes.INTEGER,
+      allowNull: false,
     },
-
     estoque: {
-        type: DataTypes.INTEGER,
-        defaultValue: 0,
-
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
     },
-
-    createdAt: { type: DataTypes.DATE },
-
-    updatedAt: { type: DataTypes.DATE },
-
-    tableName: "filme",
-
-});
+  },
+  { tableName: "filme", timestamps: false }
+);
 
 module.exports = Filme;

539ebdc69c3d9a10586110e10b40ed6820c0963a
Yuri Felipe Scabio <102414254+StaticDreamstate@users.noreply.github.com>
2022-05-19 07:38:19 -0300

    Parameter bug fixed.

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,33 +10,33 @@ const ClienteController = require("../controllers/cliente");
 
 router.get("/filme", FilmeController.index);
 router.post("/filme", FilmeController.store);
-router.get("/filme/:id", FilmeController.show);
-router.put("/filme/:id", FilmeController.update);
-router.delete("/filme/:id", FilmeController.destroy);
+router.get("/filme/:codigo", FilmeController.show);
+router.put("/filme/:codigo", FilmeController.update);
+router.delete("/filme/:codigo", FilmeController.destroy);
 
 //Rotas - Gênero:
 
 router.get("/genero", GeneroController.index);
 router.post("/genero", GeneroController.store);
-router.get("/genero/:id", GeneroController.show);
-router.put("/genero/:id", GeneroController.update);
-router.delete("/genero/:id", GeneroController.destroy);
+router.get("/genero/:codigo", GeneroController.show);
+router.put("/genero/:codigo", GeneroController.update);
+router.delete("/genero/:codigo", GeneroController.destroy);
 
 
 //Rotas - Reserva:
 
 router.get("/reserva", ReservaController.index);
 router.post("/reserva", ReservaController.store);
-router.get("/reserva/:id", ReservaController.show);
-router.put("/reserva/:id", ReservaController.update);
-router.delete("/reserva/:id", ReservaController.destroy);
+router.get("/reserva/:codigo", ReservaController.show);
+router.put("/reserva/:codigo", ReservaController.update);
+router.delete("/reserva/:codigo", ReservaController.destroy);
 
 //Rotas - Cliente
 
 router.get("/cliente", ClienteController.index);
 router.post("/cliente", ClienteController.store);
-router.get("/cliente/:id", ClienteController.show);
-router.put("/cliente/:id", ClienteController.update);
-router.delete("/cliente/:id", ClienteController.destroy);
+router.get("/cliente/:codigo", ClienteController.show);
+router.put("/cliente/:codigo", ClienteController.update);
+router.delete("/cliente/:codigo", ClienteController.destroy);
 
 module.exports = router;

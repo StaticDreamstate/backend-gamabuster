9323eabe733dfd0490832968705ca45ae7d6453d
Yuri Felipe Scabio <102414254+StaticDreamstate@users.noreply.github.com>
2022-05-19 07:28:38 -0300

    Table name fix

--- src/controllers/cliente.js
+++ src/controllers/cliente.js
@@ -1,30 +1,167 @@
+const { Cliente, Endereco } = require("../models");
+
 const ClienteController = {
-    index: (req, res) => {
-      res.json([]);
-    },
-    store: (req, res) => {
-      res.json(req.body);
-    },
-    show: (req, res) => {
-      const { id } = req.params;
-  
-      res.json({
-        id,
-        name: `Cliente ${id}`,
-        price: 300,
+  index: async (req, res) => {
+    try {
+      const allClientes = await Cliente.findAll({ include: Endereco });
+
+      res.json(allClientes);
+    } catch (error) {
+      console.log(error.message);
+      res
+        .status(500)
+        .json({ error: "Oops, tivemos um erro, tente novamente." });
+    }
+  },
+  store: async (req, res) => {
+    try {
+      const {
+        nome,
+        sobrenome,
+        cpf,
+        telefone,
+        email,
+        data_nascimento,
+        endereco = {},
+      } = req.body;
+
+      const { logradouro, numero, bairro, cidade, estado, cep } = endereco;
+
+      const novoCliente = await Cliente.create(
+        {
+          nome,
+          sobrenome,
+          cpf,
+          telefone,
+          email,
+          data_nascimento,
+          Endereco: {
+            logradouro,
+            numero,
+            bairro,
+            cidade,
+            estado,
+            cep,
+          },
+        },
+        { include: [Endereco] }
+      );
+
+      // await Endereco.create({
+      //   logradouro,
+      //   numero,
+      //   bairro,
+      //   cidade,
+      //   estado,
+      //   cep,
+      //   cliente_id: novoCliente.codigo,
+      // });
+
+      res.json(novoCliente);
+    } catch (error) {
+      console.log(error.message);
+      res
+        .status(500)
+        .json({ error: "Oops, tivemos um erro, tente novamente." });
+    }
+  },
+  show: async (req, res) => {
+    const { id } = req.params;
+
+    try {
+      const cliente = await Cliente.findByPk(id, { include: Endereco });
+
+      if (cliente) {
+        return res.json(cliente);
+      }
+
+      res.status(404).json({
+        message: "Cliente não encontrado",
       });
-    },
-    update: (req, res) => {
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "Oops, tivemos um erro, tente novamente." });
+    }
+  },
+  update: async (req, res) => {
+    try {
       const { id } = req.params;
-  
-      res.json({
-        id,
-        ...(req.body || {}),
+      const {
+        nome,
+        sobrenome,
+        cpf,
+        telefone,
+        email,
+        data_nascimento,
+        endereco = {},
+      } = req.body;
+
+      const { logradouro, numero, bairro, cidade, estado, cep } = endereco;
+
+      const cliente = await Cliente.findByPk(id, { include: Endereco });
+
+      if (!cliente) {
+        res.status(404).json({
+          message: "Cliente não encontrado",
+        });
+      }
+
+      await Cliente.update(
+        { nome, sobrenome, cpf, telefone, email, data_nascimento },
+        { where: { codigo: id } }
+      );
+
+      if (cliente.Endereco?.codigo) {
+        await Endereco.update(
+          { logradouro, numero, bairro, cidade, estado, cep },
+          { where: { codigo: cliente.Endereco.codigo } }
+        );
+      } else {
+        await Endereco.create({
+          logradouro,
+          numero,
+          bairro,
+          cidade,
+          estado,
+          cep,
+          cliente_codigo: id,
+        });
+      }
+
+      const clienteAtualizado = await Cliente.findByPk(id, {
+        include: Endereco,
       });
-    },
-    destroy: (req, res) => {
+
+      res.json(clienteAtualizado);
+    } catch (error) {
+      console.log(error.message);
+      res
+        .status(500)
+        .json({ error: "Oops, tivemos um erro, tente novamente." });
+    }
+  },
+  destroy: async (req, res) => {
+    const { id } = req.params;
+
+    try {
+      const cliente = await Cliente.findByPk(id);
+
+      if (!cliente) {
+        res.status(404).json({
+          message: "Cliente não encontrado",
+        });
+      }
+
+      await cliente.destroy();
+
       res.status(204).send("");
-    },
-  };
-  
-  module.exports = ClienteController;
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "Oops, tivemos um erro, tente novamente." });
+    }
+  },
+};
+
+module.exports = ClienteController;

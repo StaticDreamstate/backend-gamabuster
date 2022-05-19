f7428bb1948295960cf4dc824bfda7927566fb55
Yuri Felipe Scabio <102414254+StaticDreamstate@users.noreply.github.com>
2022-05-19 07:29:41 -0300

    Table name fix

--- src/controllers/genero.js
+++ src/controllers/genero.js
@@ -1,62 +1,67 @@
-const Genero = require("../models/Genero")
+const Genero = require("../models/Genero");
 
 const GeneroController = {
   
   index: async (req, res) => {
     const allGeneros = await Genero.findAll();
+
     res.json(allGeneros);
   },
-  
+
   store: async (req, res) => {
     const { nome } = req.body;
     const novoGenero = await Genero.create({ nome });
 
     res.json(novoGenero);
   },
-
+  
   show: async (req, res) => {
     const { codigo } = req.params;
+
     const genero = await Genero.findByPk(codigo);
 
     if (genero) {
-      return res.json(genero);  
+      return res.json(genero);
     }
 
     res.status(404).json({
-      message: "Gênero não encontrado",
-    })
-
+      message: "Genero não encontrado",
+    });
   },
 
   update: async (req, res) => {
-    const { codigo } = req.params;
+    const { id } = req.params;
     const { nome } = req.body;
-    
-    const genero = await Genero.findByPk(codigo);
 
-    if (!genero) { 
+    const genero = await Genero.findByPk(id);
+
+    if (!genero) {
       res.status(404).json({
-      message: "Gênero não encontrado",
-      })
+        message: "Genero não encontrado",
+      });
     }
 
     genero.nome = nome;
     await genero.save();
 
     res.json(genero);
-  },
-  
-  destroy: async (req, res) => {
-    const { codigo } = req.params;
-    const genero = await Genero.findByPk(codigo);
 
-    if (!genero) { 
+    // await Genero.update({ nome }, { where: { codigo: id } });
+
+    // res.json({ message: "Gênero atualizado com sucesso" });
+  },
+  destroy: async (req, res) => {
+    const { id } = req.params;
+    const genero = await Genero.findByPk(id);
+
+    if (!genero) {
       res.status(404).json({
-      message: "Gênero não encontrado",
-      })
+        message: "Genero não encontrado",
+      });
     }
 
     await genero.destroy();
+
     res.status(204).send("");
   },
 };
